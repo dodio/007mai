4179548dfaba924584d06392526ceab82e95b9e4
dodio <go2solo18@gmail.com>
2015-05-25 01:01:03 +0800

    特卖商城

---
+++ app/Lib/Action/index/mallAction.class.php
@@ -0,0 +1,13 @@
+<?php 
+class mallAction extends FirstendAction {
+  public function _initialize() {
+        parent::_initialize();
+  }
+
+  public function index(){
+    
+    $this->display();
+  }
+
+}
+ ?>
---
+++ app/Tpl/index/007mai/mall/index.tpl
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<include file="public:head" />
+</head>
+<body>
+<include file="public:header" />
+
+
+<include file="public:footer" />
+</body>
+</html>

b02ebd79763d790ed012ca8587b1c8a82419cd0c
dodio <go2solo18@gmail.com>
2015-06-04 02:32:37 +0800

    SEO关键字

--- app/Lib/Action/index/mallAction.class.php
+++ app/Lib/Action/index/mallAction.class.php
@@ -5,7 +5,12 @@ class mallAction extends FirstendAction {
   }
 
   public function index(){
-    
+    $this->assign("nav_curr","mall");
+    $this->_config_seo(array(
+      "title" => "特卖商城，品牌团",
+      "keywords" => "品牌团,特卖商城,全网活动,聚划算",
+      "description" => "007买为您收集全网最优惠的活动，满满的都是爱"
+      ));
     $this->display();
   }
 

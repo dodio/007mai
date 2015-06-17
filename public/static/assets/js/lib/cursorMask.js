bc8e153aff3927f2944a83fe72b7f7fa8e3cdd98
dodio <go2solo18@gmail.com>
2015-06-17 10:01:54 +0800

    静态资源移到网站目录

---
+++ public/static/assets/js/lib/cursorMask.js
@@ -0,0 +1,87 @@
+
+(function($){
+  var pluginName = "cursorMask";
+
+  function Plugin(ele,conf){
+    this.init(ele,conf);
+  }
+
+  Plugin.prototype.init = function(ele,conf) {
+    this.ele = ele =  $(ele);
+    var offset = this.offset = ele.offset();
+    this.width = ele.width();
+    this.height = ele.height();
+    this.mask = $(".bk-link",ele);
+
+    this.left = offset.left;
+    this.top = offset.top;
+    this.bottom = offset.top + this.height;
+    this.right = offset.left + this.width;
+
+    this.conf = $.extend({
+      speed:300
+    },conf);
+    this.bind();
+  };
+
+  
+  var normal = {left:0,top:0};
+
+  Plugin.prototype.bind = function() {
+    var _self = this;
+    this.ele.bind("mouseenter."+ pluginName ,function(e){
+
+      _self.mask.parent().show();
+      var dir = direction.call(_self,e);
+      var pos = position(_self.width,_self.height,dir);
+      _self.mask.css(pos).animate(normal,_self.conf.speed);
+
+    });
+    this.ele.bind("mouseleave."+pluginName,function(e){
+
+      var dir = direction.call(_self,e);
+      var pos = position(_self.width,_self.height,dir);
+      _self.mask.css(normal).animate(pos,_self.conf.speed,function(){
+        _self.mask.parent().hide();
+      });
+    });
+  };
+
+  Plugin.prototype.unbind = function() {
+    this.ele.unbind("."+pluginName);
+  };
+
+  //获取进入方向
+  function direction(e){
+    var w = this.width;
+    var h = this.height;
+    var x = (e.pageX - this.left - (w / 2)) * (w > h ? (h / w) : 1);
+    var y = (e.pageY - this.top - (h / 2)) * (h > w ? (w / h) : 1);
+    var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
+    return direction;
+  }
+  
+  function position(width,height,dir){
+    var position = [
+      // 上右下左
+      {left:0,top:-height},
+      {left:width,top:0},
+      {left:0,top:height},
+      {left:-width,top:0}
+    ]
+    return position[dir];
+  }
+
+  $.fn[pluginName] = function(conf){
+    var objs = this.map(function(){
+      var _self = $(this);
+      if(_self.data(pluginName+'_obj'))
+        return _self.data(pluginName+'_obj');
+
+      var obj = new Plugin(this,conf);
+      _self.data(pluginName+'_obj',obj);
+      return obj;
+    });
+    return objs.length == 1 ? objs[0] : this;
+  };
+})(jQuery);
---
+++ public/static/assets/js/lib/scrollmanager.js
@@ -0,0 +1,106 @@
+(function($){
+
+    var calls = [];
+
+    // var call_obj = {
+    //     height:300, //检测的值（只在越过的时候检测）
+    //     up:$.noop, 
+    //     down:$.noop,
+    //     everywhere:false
+    // }
+
+    var ot = 0;
+    var doc = $(document);
+    var conf = {
+        delay:50
+    }
+    function handleScroll(){
+        var st = doc.scrollTop();
+        if(st === ot)
+            return;
+        $.each(calls,function(i,o){
+            dealCalls(o,st,ot)
+        });
+        ot = st;
+    }
+
+    function dealCalls(o,st,ot,firsttime){
+        if(o.height){
+            if( st >= o.height){
+                if(o.everywhere || !o.last || o.last === "up" || firsttime ){
+                    $.isFunction(o.down) && o.down(st,ot);
+                    o.last = "down";
+                }
+                return ;
+            }else{
+                if(o.everywhere || !o.last || o.last === "down" || firsttime ){
+                    $.isFunction(o.up) && o.up(st,ot);
+                    o.last = "up";
+                }
+                return ;
+            }
+        }
+
+        if(!o.height ){
+            if(st >= ot ){
+                if(!o.last || o.last === "up"){
+                    $.isFunction(o.down) && o.down(st,ot);
+                    o.last = "down";
+                }
+                return ;
+            }else{
+                if(!o.last || o.last === "down"){
+                    $.isFunction(o.up) && o.up(st,ot);
+                    o.last = "up";
+                }
+                return ;
+            }
+        }
+
+    }
+
+    var thread = 0 ;
+    function scroll(){
+        clearTimeout(thread);
+        setTimeout(function(){
+            handleScroll();
+        },conf.delay);
+    }
+
+    var scrollManager = {
+        bind:function(){
+            $(window).bind("scroll.scrollManager",scroll);
+        },
+        unbind:function(){
+            $(window).unbind(".scrollManager");
+        },
+        conf:conf,
+        clear:function(){
+            calls = [];
+        },
+        add:function(obj){
+            obj = $.extend({
+                up:$.noop, 
+                down:$.noop,
+                everywhere:false},obj
+                );
+
+            var st = doc.scrollTop();
+
+            dealCalls(obj,st,ot,true);
+
+            calls.push(obj);
+        },
+        remove:function(obj){
+            for(var i in calls){
+                if(calls[i] === obj){
+                    return calls.slice(i,1);
+                }
+            }
+        }
+    }
+    scrollManager.bind();
+    $.scrollManager = scrollManager;
+
+  
+})(jQuery);

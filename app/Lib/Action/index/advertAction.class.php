0c7afbf0755fa38015ccab0337e5b23c8abcff22
dodio <go2solo18@gmail.com>
2015-06-22 04:13:09 +0800

    获取广告位内容的action

--- app/Lib/Action/index/advertAction.class.php
+++ app/Lib/Action/index/advertAction.class.php
@@ -8,4 +8,16 @@ class advertAction extends FirstendAction {
         !$url && $this->_404();
         redirect($url);
     }
+
+    public function get(){
+      $id = I("id",0,'intval');
+      $tracker = I("tracker",'','trim');
+
+      if($id != 0){
+        R('advert/index', array($id), 'Widget');
+      }
+      if($tracker != "" ){
+        R('advert/index', array($tracker), 'Widget');
+      }
+    }
 }

8b25f9af4d3f8713dc7a2b2a44f60f5b1d213e62
dodio <go2solo18@gmail.com>
2015-06-26 00:56:31 +0800

    构建时获取tpl_list

--- app/Lib/Action/admin/adboardAction.class.php
+++ app/Lib/Action/admin/adboardAction.class.php
@@ -3,11 +3,10 @@ class adboardAction extends BackendAction {
     public function _initialize() {
         parent::_initialize();
         $this->_mod = D('adboard');
+        $this->assign('tpl_list', $this->_mod->get_tpl_list());
     }
 
     public function _before_index() {
-        $tpl_list = $this->_mod->get_tpl_list();
-        $this->assign('tpl_list', $tpl_list);
         $big_menu = array(
             'title' => L('adboard_add'),
             'iframe' => U('adboard/add'),
@@ -29,11 +28,6 @@ class adboardAction extends BackendAction {
         return $map;
     }
 
-    protected function _before_add() {
-        $tpl_list = $this->_mod->get_tpl_list();
-        $this->assign('tpl_list', $tpl_list);
-    }
-
     protected function _before_insert($data) {
         if ($this->_mod->name_exists($data['name'])) {
             $this->ajaxReturn(0, L('adboard_already_exists'));
@@ -44,7 +38,6 @@ class adboardAction extends BackendAction {
         $tpl_list = $this->_mod->get_tpl_list();
         $this->assign('tpl_list', $tpl_list);
     }
-
     protected function _before_update($data) {
         if ($this->_mod->name_exists($data['name'], $data['id'])) {
             $this->ajaxReturn(0, L('adboard_already_exists'));

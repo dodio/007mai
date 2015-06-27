d6e41b70efa3fe6d25172e5ea97d6af75a1ecbee
dodio <go2solo18@gmail.com>
2015-06-27 16:06:33 +0800

    MD5加密一下

--- app/Lib/Action/admin/cacheAction.class.php
+++ app/Lib/Action/admin/cacheAction.class.php
@@ -36,10 +36,10 @@ class cacheAction extends BackendAction
                 break;
             case 'js':
                 is_dir(ROOT_PATH . '/static/dist_js/') && $obj_dir->del(ROOT_PATH . '/static/dist_js/');
-                F("clear_time_js",time(),RUNTIME_PATH);
+                F("clear_time_js",md5(time()),RUNTIME_PATH);
                 break;
             case 'css':
-                F("clear_time_css",time(),RUNTIME_PATH);
+                F("clear_time_css",md5(time()),RUNTIME_PATH);
                 $this->_clear_dist_css($obj_dir);
                 break;
 			case 'api':
@@ -63,9 +63,9 @@ class cacheAction extends BackendAction
 		is_dir(RUNTIME_PATH . '/TaobaoAPI/') && $obj_dir->delDir(RUNTIME_PATH . '/TaobaoAPI/');
         is_dir(LOG_PATH) && $obj_dir->delDir(LOG_PATH);
         is_dir(ROOT_PATH . '/static/dist_js/') && $obj_dir->del(ROOT_PATH . '/static/dist_js/');
-        F("clear_time_js",time(),RUNTIME_PATH);
+        F("clear_time_js",md5(time()),RUNTIME_PATH);
         $this->_clear_dist_css($obj_dir);
-        F("clear_time_css",time(),RUNTIME_PATH);
+        F("clear_time_css",md5(time()),RUNTIME_PATH);
         @unlink(RUNTIME_FILE);
         $this->ajaxReturn(1, L('clear_success'));
     }

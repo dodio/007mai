41489ad5050db62f27edb42bd7b5ea429c97ce5b
dodio <go2solo18@gmail.com>
2015-07-06 18:33:51 +0800

    根据jquerycookie作的修改

--- public/static/assets/js/common/stat.js
+++ public/static/assets/js/common/stat.js
@@ -17,14 +17,7 @@
 (function($){
   var zeroDate = $.MAI007.util.zeroDate;
   var cookie_name = "MAI007_USER_ACTIVE";
-  var useractive = $.cookie(cookie_name,function(value){
-    try{
-      return JSON.parse(value);
-    }catch(e){
-      return;
-    }
-  });
-
+  var useractive = $.cookie(cookie_name);
   useractive = useractive || {
     time:0,
     days:0,
@@ -34,7 +27,7 @@
   };
   if(useractive.time > zeroDate()){
     //今天已经记录过
-    // return ;
+    return ;
   }
   var day_time = 86400000;
   // 今日未记录
@@ -60,14 +53,8 @@
   }
   var fsd = new Date();
   fsd.setTime(useractive.time);
-  _czc.push(﻿["_trackEvent","用户情况","留存", useractive.totalDays ,fsd.getHours()]);
-  _czc.push(﻿["_trackEvent","用户情况",'连续登陆',useractive.continueDays, fsd.getHours()]);
-  _czc.push(﻿["_trackEvent","用户情况",'来访天数',useractive.days, ((useractive.days/useractive.totalDays).toFixed(2)*100) ]);
-
-  if(!JSON){
-    $.getScript(CDN_ROOT+"/assets/js/lib/json.js",function(){
-      $.cookie(cookie_name,JSON.stringify(useractive),{path:"/",expires:365});
-    });
-  }
-  $.cookie(cookie_name,JSON.stringify(useractive),{path:"/",expires:365});
+  _czc.push(["_trackEvent","用户情况","留存", useractive.totalDays ,fsd.getHours()]);
+  _czc.push(["_trackEvent","用户情况",'连续登陆',useractive.continueDays, fsd.getHours()]);
+  _czc.push(["_trackEvent","用户情况",'来访天数',useractive.days, ((useractive.days/useractive.totalDays).toFixed(2)*100) ]);
+  $.cookie(cookie_name,useractive,{path:"/",expires:365});
 })(jQuery);
